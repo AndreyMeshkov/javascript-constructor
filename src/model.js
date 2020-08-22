f2d216982b20591cf1989fae8a677496f15bd5a5
Andrei Miashkou <meshkow.andrei@gmail.com>
2020-08-22 14:14:16 +0300

    feat: add files: sidebar.js and site.js

--- src/model.js
+++ src/model.js
@@ -1,14 +1,20 @@
 import image from './assets/image.png'
 import { TextBlock, TitleBlock, ImageBlock, TextColumnsBlock } from './classes/blocks'
+import { css } from './utils'
 
 const text = `
-	Создание сайтов при помощи конструктора
+Создание сайтов при помощи конструктора
 `
 
 export const model = [
 	new TitleBlock('Конструктор сайтов на чистом JavaScript!', {
 		tag: 'h2',
-		styles: 'background: linear-gradient(to right, #ff0099, #493240);color: #fff;padding: 1.5rem;text-align: center;'
+		styles: css({
+			background: 'linear-gradient(to right, #ff0099, #493240)',
+			color: '#fff',
+			padding: '1.5rem',
+			'text-align': 'center'
+		})
 	}),
 	new ImageBlock(image, {
 		styles: 'padding: 2rem 0;display: flex;justify-content: center;',
